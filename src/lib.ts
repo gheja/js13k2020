f75c5def108424dc1dc630a0e7a2877c5775baa3
gheja <gheja@w-7b.git.stdio.hu>
2020-09-08 01:09:21 +0000

    lib: added more library functions

--- src/lib.ts
+++ src/lib.ts
@@ -1,12 +1,19 @@
-/*
-function distance3D(p1: tPoint3D, p2: tPoint3D)
-{
-    // manhattan distance to speed things up
-    return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y) + Math.abs(p1.z - p2.z);
-}
-*/
-function distance3D(p1: tPoint3D, p2: tPoint3D)
+function distance3D(p1: tPoint3D, p2: tPoint3D): number
 {
     // manhattan distance to speed things up
     return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]) + Math.abs(p1[2] - p2[2]);
+}
+
+function getAngle2D(p1: tPoint3D, p2: tPoint3D): number
+{
+    return Math.atan2(p2[1] - p1[1], p2[0] - p1[0]);
+}
+
+function offset3D(p: tPoint3D, angle: number, distance: number, height: number): tPoint3D
+{
+    return F32A([
+        p[0] + Math.cos(angle + Math.PI/2) * distance,
+        p[1] + Math.sin(angle + Math.PI/2) * distance,
+        p[2] + height
+    ]);
 }
