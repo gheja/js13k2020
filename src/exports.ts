548d685fac1d37e2898dec7ba904dca1d881e9e0
gheja <gheja@w-7b.git.stdio.hu>
2020-09-12 21:14:35 +0000

    Moved exports to exports.ts

---
+++ src/exports.ts
@@ -0,0 +1,13 @@
+// "exports" so google closure compiler won't break JS access from HTML
+window["setToolInfo"] = setToolInfo;
+window["setToolDelete"] = setToolDelete;
+window["setToolRoad"] = setToolRoad;
+window["openBank"] = openBank;
+window["openStats"] = openStats;
+window["loan1"] = loan1;
+window["loan2"] = loan2;
+window["scheduleDelete"] = scheduleDelete;
+window["scheduleAppend"] = scheduleAppend;
+window["scheduleSkip"] = scheduleSkip;
+window["vehiclePause"] = vehiclePause;
+window["vehicleDepot"] = vehicleDepot;
--- src/main.ts
+++ src/main.ts
@@ -273,17 +273,3 @@ function init()
 }
 
 window.addEventListener("load", init);
-
-// "exports" so google closure compiler won't break JS access from HTML
-window["setToolInfo"] = setToolInfo;
-window["setToolDelete"] = setToolDelete;
-window["setToolRoad"] = setToolRoad;
-window["openBank"] = openBank;
-window["openStats"] = openStats;
-window["loan1"] = loan1;
-window["loan2"] = loan2;
-window["scheduleDelete"] = scheduleDelete;
-window["scheduleAppend"] = scheduleAppend;
-window["scheduleSkip"] = scheduleSkip;
-window["vehiclePause"] = vehiclePause;
-window["vehicleDepot"] = vehicleDepot;
