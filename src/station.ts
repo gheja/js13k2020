6abfad673b97a9d65fe78033459778bfbc84bc24
gheja <gheja@w-7b.git.stdio.hu>
2020-09-13 01:01:41 +0000

    Station: it now can be a Depot too

--- src/station.ts
+++ src/station.ts
@@ -8,16 +8,47 @@ class Station
     factoriesInRange: Array<Factory>;
     webglGfxObject: any;
     title: string;
+    isDepot: boolean;
+    roadParts: Array<tNetworkNode>;
 
-    constructor(position)
+    constructor(position: tPoint3D, isDepot: boolean)
     {
         this.stationIndex = (_stationIndexSequence++);
         this.position = position;
-        this.range = 5;
+        this.isDepot = isDepot;
+        this.roadParts = [];
 
-        this.title = `Station ${this.stationIndex}`;
+        let p1, p2, p3;
 
-        this.webglGfxObject = _gfx.createObject(SHAPE_STATION_INDEX),
+        p2 = _roads.addNode([ this.position[0], this.position[1], this.position[2] ], true, this);
+
+        if (isDepot)
+        {
+            this.range = 0;
+            this.title = `Depot ${this.stationIndex}`;
+            this.webglGfxObject = _gfx.createObject(SHAPE_ROAD_DEPOT_INDEX);
+
+            p1 = _roads.addNode([ this.position[0], this.position[1] + 2, this.position[2] ], true);
+            p3 = _roads.addNode([ this.position[0], this.position[1] - 10, this.position[2] ], true);
+
+        }
+        else
+        {
+            this.range = 5;
+            this.title = `Station ${this.stationIndex}`;
+            this.webglGfxObject = _gfx.createObject(SHAPE_ROAD_BUS_STOP_INDEX);
+
+            p1 = _roads.addNode([ this.position[0], this.position[1] + 8, this.position[2] ], true);
+            p3 = _roads.addNode([ this.position[0], this.position[1] - 8, this.position[2] ], true);
+        }
+
+
+        _roads.addEdge(p1, p2);
+        _roads.addEdge(p2, p3);
+
+        this.roadParts.push(p1, p2, p3);
+
+        _roads.rebuildGfx();
 
         this.webglGfxObject.x = this.position[0];
         this.webglGfxObject.y = this.position[1];
